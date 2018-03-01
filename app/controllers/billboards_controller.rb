63e006e19788b99b59ab2f00d811168e27c02569
Bradley Findeis <bradleyfindeis@gmail.com>
2018-03-01 16:33:00 -0700

    Billboard feature created

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,13 +1,52 @@
 class BillboardsController < ApplicationController
+  before_action :set_billboard, only: [:show, :edit, :update, :destroy]
+
   def index
+    @billboards = Billboard.all  
   end
 
   def show
   end
 
   def new
+    @billboard = Billboard.new 
   end
 
   def edit
   end
+
+  def create 
+    @billboard = Billboard.new(billboard_params)
+
+    if @billboard.save
+      redirect_to billboards_path
+    else 
+      render :new
+    end 
+  end 
+
+  def update 
+    if @billboard.update(billboard_params)
+      redirect_to @billboards
+    else 
+      render :edit 
+    end 
+  end 
+
+  def destroy 
+    if @billboards.destroy
+      redirect_to billboards_path
+  end 
+
+
+  private
+
+  def set_billboard
+    @billobard = Billboard.find(params[:id])
+  end 
+
+  def billboard_params
+    params.require(:billboard).permit(:name)
+  end 
+  
 end
