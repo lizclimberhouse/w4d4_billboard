5c904be08869d43cf66be3fabe1bb7c7bd9fba3d
Bradley Findeis <bradleyfindeis@gmail.com>
2018-03-01 21:53:23 -0700

    added the billboard song show feature

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,11 +1,25 @@
 class BillboardsController < ApplicationController
-  before_action :set_billboard, only: [:show, :edit, :update, :destroy]
+  before_action :set_billboard, only: [:show, :billboard_songs, :edit, :update, :destroy]
 
   def index
     @billboards = Billboard.all  
   end
 
   def show
+    # @allsongs = Song.all #all songs period
+    @selected_songs = Song.where(billboard_id: @billboard.id)
+    @all_available_songs = Song.where(billboard_id: nil) #all songs not attached to this billboard
+    # @songs = @billboard.songs #all songs attached to this billboard
+  end
+
+  def billboard_songs
+    # loop through and update the songs to have the billboard id
+    if params[:selected_songs]
+      Song.where(id: params[:selected_song_ids]).update_all(billboard_id: nil)
+    else
+      Song.where(id: params[:song_ids]).update_all(billboard_id: @billboard.id)
+    end
+    redirect_to billboard_path(@billboard.id)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'billboards#index'
-  
-  
-
+  put "/billboard_songs", to: "billboards#billboard_songs", as: :billboard_songs
   resources :billboards
 
   resources :artists do
