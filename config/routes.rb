99056b6772c639fd88c2d1bbb1d01be00998cbad
Bradley Findeis <bradleyfindeis@gmail.com>
2018-03-02 15:22:19 -0700

    started the devise portion

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'billboards#index'
+  root 'accounts#index'
+  devise_for :users
   put "/billboard_songs", to: "billboards#billboard_songs", as: :billboard_songs
   resources :billboards
+  resources :accounts
 
   resources :artists do
     resources :songs
